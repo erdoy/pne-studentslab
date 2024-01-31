a1b7f05bc66e1d5448761906065c976570503159
edgamen <e.gaya.2023@alumnos.urjc.es>
2024-01-31 14:49:38 +0100

    This is the first change

--- P01/e10.py
+++ P01/e10.py
@@ -3,12 +3,8 @@ from Seq1 import *
 FOLDER = "/home/alumnos/edgamen/Escritorio/PNE/sequences/"
 FILENAMES = ["U5", "ADA", "FRAT1", "FXN", "RNU6_269P"]
 
-seqs = []
+s = Seq()
 
 for i in FILENAMES:
-    s = Seq()
     s.read_fasta(FOLDER + i + ".txt")
-    seqs.append(s)
     print(f"Gene {i}: Most frequent Base: {str(max(s.count(), key = s.count().get))} ")
-
-
