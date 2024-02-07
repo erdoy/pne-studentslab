15d5213face3f9af1fe1735f6732a71be6d690c1
edgamen <e.gaya.2023@alumnos.urjc.es>
2024-02-07 17:49:35 +0100

    update

---
+++ S10/echo-server3.py
@@ -0,0 +1,77 @@
+import socket, termcolor
+
+# Configure the Server's IP and PORT
+PORT = 8080
+IP = "212.128.255.68" # the IP address depends on the machine running the server
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
+nclient = 0
+client_list = []
+
+while nclient < 5:
+
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect")
+
+    try:
+        (cs, client_ip_port) = ls.accept()
+
+    # -- Server stopped manually
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
+
+        # -- Close the listening socket
+        ls.close()
+
+        # -- Exit!
+        exit()
+
+    # -- Execute this part if there are no errors
+    else:
+
+        nclient += 1
+        client_list.append(client_ip_port)
+
+        print(f"CONNECTION {nclient}. Client IP, PORT: ('{client_ip_port[0]}', {client_ip_port[1]})")
+
+        print("A client has connected to the server!")
+
+        # -- Read the message from the client
+        # -- The received message is in raw bytes
+        msg_raw = cs.recv(2048)
+
+        # -- We decode it for converting it
+        # -- into a human-readable string
+        msg = msg_raw.decode()
+
+        # -- Print the received message
+        print("Received Message: ", end="")
+        termcolor.cprint(msg, "green", force_color=True)
+
+        # -- Send a response message to the client
+        response = "ECHO: " + msg + "\n"
+
+        # -- The message has to be encoded into bytes
+        cs.send(response.encode())
+
+        # -- Close the client socket
+        cs.close()
+
+# -- Close the listening socket
+ls.close()
+
+for i in range(len(client_list)):
+    print(f"Client {i}: ({client_list[i][0]}, {client_list[i][1]})")
