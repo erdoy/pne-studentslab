8e08a3647798005e98b34a46b93ba91cbe798864
edgamen <e.gaya.2023@alumnos.urjc.es>
2024-02-15 14:30:19 +0100

    update

--- S16/echo-server-e1.py
+++ S16/echo-server-e1.py
@@ -21,27 +21,29 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         # Print the request line
         termcolor.cprint(self.requestline, 'green', force_color=True)
 
-        from urllib.parse import parse_qs, urlparse
-        url_path = urlparse(self.path)
-        path = url_path.path  # we get it from here
-        arguments = parse_qs(url_path.query)
+        # from urllib.parse import parse_qs, urlparse
+        # url_path = urlparse(self.path)
+        # path = url_path.path  # we get it from here
+        # arguments = parse_qs(url_path.query)
+        #
+        # import jinja2 as j
+        # def read_html_file(filename):
+        #     contents = Path("html/" + filename).read_text()
+        #     contents = j.Template(contents)
+        #     return contents
 
-        import jinja2 as j
-        def read_html_file(filename):
-            contents = Path("html/" + filename).read_text()
-            contents = j.Template(contents)
-            return contents
-
-        contents = read_html_file("form-e2.html").render(
-            context={"todisplay": "icgygc"})  # provide a dictionary to build the form
+        # contents = read_html_file("form-e2.html").render(
+        #     context={"todisplay": "icgygc"})  # provide a dictionary to build the form
 
         # Open the form1.html file
         # Read the index from the file
-        # contents = Path("html/form-1.html").read_text()
-        # if self.requestline.startswith("GET /echo?msg="):
-        #     msg = self.requestline.strip("GET /echo?msg=").strip(" HTTP/1.1")
-        #     contents = Path('html/form-e1.html').read_text().format(msg)
-        #     termcolor.cprint(msg, 'red', force_color=True)
+        contents = Path("html/form-1.html").read_text()
+        if self.requestline.startswith("GET /echo?msg="):
+            msg = self.requestline.strip("GET /echo?msg=").strip(" HTTP/1.1")
+            if "&chk=on" in self.requestline:
+                msg = self.requestline.strip("GET /echo?msg=").strip("&chk=on HTTP/1.1").upper()
+            contents = Path('html/form-e1.html').read_text().format(msg)
+            termcolor.cprint(msg, 'red', force_color=True)
 
         # Generating the response message
         self.send_response(200)  # -- Status line: OK!
