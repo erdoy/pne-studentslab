17a9604fdc3f5c97819f5afc8aa43778925cba74
edgamen <e.gaya.2023@alumnos.urjc.es>
2024-04-23 16:30:41 +0200

    update

--- S16/echo-server-e1.py
+++ S16/echo-server-e1.py
@@ -2,12 +2,19 @@ import http.server
 import socketserver
 import termcolor
 from pathlib import Path
+import jinja2 as j
 
 # Define the Server's port
 PORT = 8080
 
 # -- This is for preventing the error: "Port already in use"
 socketserver.TCPServer.allow_reuse_address = True
+
+
+def read_html_file(filename):
+    contents = Path("html/" + filename).read_text()
+    contents = j.Template(contents)
+    return contents
 
 
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
@@ -19,31 +26,24 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         in the HTTP protocol request"""
 
         # Print the request line
-        termcolor.cprint(self.requestline, 'green', force_color=True)
+        termcolor.cprint(self.requestline, 'green')
 
-        # from urllib.parse import parse_qs, urlparse
-        # url_path = urlparse(self.path)
-        # path = url_path.path  # we get it from here
-        # arguments = parse_qs(url_path.query)
-        #
-        # import jinja2 as j
-        # def read_html_file(filename):
-        #     contents = Path("html/" + filename).read_text()
-        #     contents = j.Template(contents)
-        #     return contents
+        from urllib.parse import parse_qs, urlparse
+        url_path = urlparse(self.path)
+        path = url_path.path  # we get it from here
+        arguments = parse_qs(url_path.query)
 
-        # contents = read_html_file("form-e2.html").render(
-        #     context={"todisplay": "icgygc"})  # provide a dictionary to build the form
-
-        # Open the form1.html file
-        # Read the index from the file
-        contents = Path("html/form-1.html").read_text()
-        if self.requestline.startswith("GET /echo?msg="):
-            msg = self.requestline.strip("GET /echo?msg=").strip(" HTTP/1.1")
-            if "&chk=on" in self.requestline:
-                msg = self.requestline.strip("GET /echo?msg=").strip("&chk=on HTTP/1.1").upper()
-            contents = Path('html/form-e1.html').read_text().format(msg)
-            termcolor.cprint(msg, 'red', force_color=True)
+        if path == "/":
+            contents = Path("html/form-1.html").read_text()
+        elif path == "/echo":
+            msg = arguments["msg"][0]
+            if "chk" in arguments:
+                if arguments["chk"][0] == "on":
+                    msg = arguments["msg"][0].upper()
+            contents = read_html_file("form-e1.html").render(context={"todisplay": msg})
+            termcolor.cprint(msg, 'red')
+        else:
+            contents = read_html_file("error.html").render()
 
         # Generating the response message
         self.send_response(200)  # -- Status line: OK!
--- S16/form-server1.py
+++ S16/form-server1.py
@@ -12,7 +12,7 @@ socketserver.TCPServer.allow_reuse_address = True
 
 
 # Class with our Handler. It is a called derived from BaseHTTPRequestHandler
-# It means that our class inheritates all his methods and properties
+# It means that our class inherits all his methods and properties
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
     def do_GET(self):
@@ -20,7 +20,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         in the HTTP protocol request"""
 
         # Print the request line
-        termcolor.cprint(self.requestline, 'green', force_color=True)
+        termcolor.cprint(self.requestline, 'green')
 
         # Open the form1.html file
         # Read the index from the file
@@ -59,5 +59,5 @@ with socketserver.TCPServer(("", PORT), Handler) as httpd:
         httpd.serve_forever()
     except KeyboardInterrupt:
         print("")
-        print("Stoped by the user")
+        print("Stopped by the user")
         httpd.server_close()
